ca71554731455ee3508b8321a68d51356adf4274
celioMigliara <la217853@student.helha.be>
2025-09-29 16:48:35 +0200

    Ajout du projet RAMENEtoi

---
+++ Raméne-toi/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Raméne_toi
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
