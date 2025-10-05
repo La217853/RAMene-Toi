93d7b6516604a159967a49e5c14f7a2ead07d31e
celioMigliara <la217853@student.helha.be>
2025-10-05 11:58:37 +0200

    Ajout de la classe model de Composant

---
+++ Raméne-toi/Models/Composant.cs
@@ -0,0 +1,24 @@
+﻿namespace Raméne_toi.Models
+{
+    public class Composant
+    {
+
+        public int IdCOmposant { get; set; }
+
+        public string NomComposant { get; set; }
+
+        public string TypeComposant { get; set; }
+
+        public string MarqueCOmposant { get; set; }
+
+        public string ModeleComposant { get; set; }
+
+        public decimal PrixComposant { get; set; }
+
+        public int StockCompsant { get; set; }
+
+        public double ScoreComposant { get; set; }
+
+
+    }
+}
