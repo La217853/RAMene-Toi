6bffa934a52aff6ca5327a5742695d406e8d2da5
LA238632-Devreux <la238632@student.helha.be>
2025-10-06 09:53:47 +0200

    Ajout du modèle Adresse

---
+++ Raméne-toi/Models/Adresse.cs
@@ -0,0 +1,11 @@
+﻿namespace Raméne_toi.Models
+{
+    public class Adresse
+    {
+        public int IdAdresse { get; set; }
+        public string CodeAdresse { get; set; }
+        public int NumeroAdresse { get; set; }
+        public string RueAdresse { get; set; }
+    
+    }
+}
