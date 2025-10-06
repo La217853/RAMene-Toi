9598d0c8ce216f94683f5315f58446a3e004cd61
LA238632-Devreux <la238632@student.helha.be>
2025-10-06 09:37:49 +0200

    Ajout du modèle Utilisateur

---
+++ Raméne-toi/Models/Utilisateur.cs
@@ -0,0 +1,11 @@
+﻿namespace Raméne_toi.Models
+{
+    public class Utilisateur
+    {
+        public int Id_utilisateur { get; set; }
+        public string nom_utilisateur { get; set; }
+        public string prenom_utilisateur { get; set; }
+        public string email_utilisateur { get; set; }
+        public string mdp_utilisateur { get; set; }
+    }
+}
