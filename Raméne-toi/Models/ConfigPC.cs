af14d39435a613680aa2a5d5b803e3b179b8e786
LA217383-Pirson <la217383@student.helha.be>
2025-10-12 19:43:12 +0200

    Ajout de l'interface de configPc

---
+++ Raméne-toi/Models/ConfigPC.cs
@@ -0,0 +1,8 @@
+﻿namespace Raméne_toi.Models
+{
+    public class ConfigPC
+    {
+        public int Id_configuration_pc { get; set; }
+        public string nom_configuration_pc { get; set; }
+    }
+}
