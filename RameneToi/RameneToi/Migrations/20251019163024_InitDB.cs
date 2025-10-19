67fe1d74d7d45284778ca306e5c7a90ed9c9222c
celioMigliara <la217853@student.helha.be>
2025-10-19 18:32:47 +0200

    Premiere migration

---
+++ RameneToi/RameneToi/Migrations/20251019163024_InitDB.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RameneToi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Adresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<int>(type: "int", nullable: false),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Rue = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Composants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Marque = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modele = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prix = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Stock = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Score = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Composants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Utilisateurs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MotDePasse = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AdresseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utilisateurs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Utilisateurs_Adresses_AdresseId",
+                        column: x => x.AdresseId,
+                        principalTable: "Adresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConfigurationPcs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomConfiguration = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UtilisateurId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConfigurationPcs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ConfigurationPcs_Utilisateurs_UtilisateurId",
+                        column: x => x.UtilisateurId,
+                        principalTable: "Utilisateurs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Commandes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UtilisateurId = table.Column<int>(type: "int", nullable: false),
+                    ConfigurationPcId = table.Column<int>(type: "int", nullable: false),
+                    PrixConfiguration = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commandes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Commandes_ConfigurationPcs_ConfigurationPcId",
+                        column: x => x.ConfigurationPcId,
+                        principalTable: "ConfigurationPcs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Commandes_Utilisateurs_UtilisateurId",
+                        column: x => x.UtilisateurId,
+                        principalTable: "Utilisateurs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "est_composé_de",
+                columns: table => new
+                {
+                    ComposantsId = table.Column<int>(type: "int", nullable: false),
+                    ConfigurationsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_est_composé_de", x => new { x.ComposantsId, x.ConfigurationsId });
+                    table.ForeignKey(
+                        name: "FK_est_composé_de_Composants_ComposantsId",
+                        column: x => x.ComposantsId,
+                        principalTable: "Composants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_est_composé_de_ConfigurationPcs_ConfigurationsId",
+                        column: x => x.ConfigurationsId,
+                        principalTable: "ConfigurationPcs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commandes_ConfigurationPcId",
+                table: "Commandes",
+                column: "ConfigurationPcId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commandes_UtilisateurId",
+                table: "Commandes",
+                column: "UtilisateurId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConfigurationPcs_UtilisateurId",
+                table: "ConfigurationPcs",
+                column: "UtilisateurId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_est_composé_de_ConfigurationsId",
+                table: "est_composé_de",
+                column: "ConfigurationsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Utilisateurs_AdresseId",
+                table: "Utilisateurs",
+                column: "AdresseId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commandes");
+
+            migrationBuilder.DropTable(
+                name: "est_composé_de");
+
+            migrationBuilder.DropTable(
+                name: "Composants");
+
+            migrationBuilder.DropTable(
+                name: "ConfigurationPcs");
+
+            migrationBuilder.DropTable(
+                name: "Utilisateurs");
+
+            migrationBuilder.DropTable(
+                name: "Adresses");
+        }
+    }
+}
