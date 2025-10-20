ca2070ccfabd7b38d6c8e9978834d03c04503bd3
celioMigliara <la217853@student.helha.be>
2025-10-20 16:10:30 +0200

    update

--- RameneToi/RameneToi/Data/RameneToiWebAPIContext.cs
+++ RameneToi/RameneToi/Data/RameneToiWebAPIContext.cs
@@ -29,24 +29,25 @@ namespace RameneToi.Data
          
 
             // Utilisateur 1–n ConfigurationPc 
+            //
             modelBuilder.Entity<ConfigurationPc>()
                 .HasOne(cp => cp.Utilisateur)
                 .WithMany(u => u.Configurations)
-                .HasForeignKey(cp => cp.UtilisateurId)
-                .OnDelete(DeleteBehavior.Cascade);
+                .HasForeignKey(cp => cp.UtilisateurId) //clé étrangère dans ConfigurationPc
+                .OnDelete(DeleteBehavior.Cascade); // si delete utilisateur, delete configurations
 
             // Utilisateur 1–n Commande
             modelBuilder.Entity<Commande>()
                 .HasOne(c => c.Utilisateur)
                 .WithMany(u => u.Commandes)
                 .HasForeignKey(c => c.UtilisateurId)
-                .OnDelete(DeleteBehavior.Restrict); // ou NoAction
+                .OnDelete(DeleteBehavior.Restrict); // Limite la suppresion en cascade si le user a des commandes
 
             // ConfigurationPc 1–1 Commande : pas de cascade
             modelBuilder.Entity<Commande>()
                 .HasOne(c => c.ConfigurationPc)
                 .WithOne(cp => cp.Commande)
-                .HasForeignKey<Commande>(c => c.ConfigurationPcId)
+                .HasForeignKey<Commande>(c => c.ConfigurationPcId) //FK sur commande
                 .OnDelete(DeleteBehavior.Restrict); 
 
             // Many–many ConfigurationPc <-> Composant via table "est_composé_de"
@@ -61,7 +62,7 @@ namespace RameneToi.Data
         .HasOne(u => u.Adresse)
         .WithMany(a => a.Utilisateur)
         .HasForeignKey(u => u.AdresseId) // propriété FK sur Utilisateurs
-        .HasConstraintName("FK_Adresses_Utilisateurs");
+        .HasConstraintName("FK_Adresses_Utilisateurs"); //le nom  de la contrainte FK côté base de données
         }
     }
     }
