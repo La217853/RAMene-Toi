56dca6503b08eeefa069c920548932b0dc30cc92
LA238632-Devreux <la238632@student.helha.be>
2025-11-02 16:54:04 +0100

    Ajout de l'attribut Roles et modification de Prix
    Ajout d'un nouvel attribut `Roles` dans la table `Utilisateurs` de la base de données, avec une valeur par défaut de "[]". Création d'une migration pour gérer l'ajout et la suppression de cette colonne.
    
    Modification du type de données de la colonne `Prix` dans la table `Composants` de `int` à `float` (ou `real` pour SQL Server). Mise à jour des méthodes `Up` et `Down` de la migration pour refléter ce changement.
    
    Mise à jour du fichier `RameneToiWebAPIContextModelSnapshot.cs` pour inclure ces modifications.

--- RameneToi/RameneToi/Migrations/20251102135105_ajout de l'attribut roles dans utilisateurs.Designer.cs
+++ RameneToi/RameneToi/Migrations/20251102135105_ajout de l'attribut roles dans utilisateurs.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RameneToi.Data;
 
@@ -10,9 +11,11 @@ using RameneToi.Data;
 namespace RameneToi.Migrations
 {
     [DbContext(typeof(RameneToiWebAPIContext))]
-    partial class RameneToiWebAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20251102135105_ajout de l'attribut roles dans utilisateurs")]
+    partial class ajoutdelattributrolesdansutilisateurs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RameneToi/RameneToi/Migrations/20251102135105_ajout de l'attribut roles dans utilisateurs.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RameneToi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ajoutdelattributrolesdansutilisateurs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Roles",
+                table: "Utilisateurs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "[]");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Prix",
+                table: "Composants",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Roles",
+                table: "Utilisateurs");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Prix",
+                table: "Composants",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
