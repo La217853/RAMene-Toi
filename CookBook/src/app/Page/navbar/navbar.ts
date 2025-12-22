5cfbed0d7adbd36603d298c49fd22d082b2a9e5e
La217853 <la217853@gmail.com>
2025-12-21 16:16:56 -0800

    Si admin il n'a plus decouvrir et administration ds la navbar

--- CookBook/src/app/Page/navbar/navbar.ts
+++ CookBook/src/app/Page/navbar/navbar.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../../Services/auth';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
